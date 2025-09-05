ae217e6163d6930f20b43931d924224aaf034b4e
Lindemberg Sampaio <begsamp@gmail.com>
2025-09-05 16:39:42 -0300

    ✨ commit inicial do projeto

---
+++ src/app/template/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: false,
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'] //originalmente estava no singular. Constei no plurar (chatgpt)
+})
+export class LayoutComponent {
+
+}
