faa1d10cd168c1e345a1a2faee687caaf2c96895
Lindemberg Sampaio <begsamp@gmail.com>
2025-09-20 19:57:28 -0300

    ✨ criação do form de lugares

---
+++ src/app/lugares/lugar/lugar.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Categoria } from '../../categorias/categoria';
+import { CategoriaService } from '../../categorias/categoria.service';
+
+@Component({
+  selector: 'app-lugar',
+  standalone: false,
+  templateUrl: './lugar.component.html',
+  styleUrl: './lugar.component.scss'
+})
+
+
+export class LugarComponent implements OnInit {
+
+  camposForm: FormGroup;
+  categorias: Categoria[] = [];
+
+  constructor(private categoriaService: CategoriaService) {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categoria: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
+      
+    });
+  }
+
+  ngOnInit(): void {
+    this.categoriaService.listarTodas().subscribe({
+      next: (listaCategorias) => this.categorias = listaCategorias
+    })
+  }
+
+
+  salvar(){
+
+    this.camposForm.markAllAsTouched();
+
+    if(this.camposForm.valid){
+      console.log(this.camposForm.value);
+    }
+
+  }
+
+
+  isCampoInvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return campo?.invalid && campo?.touched && campo?.errors?.['required'];
+  }
+
+}
